8eb3f4b4c554fd90eee5d68c145ce47ddeede288
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-05 17:32:50 +0200

    feat: Add guest and auth guards to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestGuard } from './auth/guest.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -12,10 +14,14 @@ const routes: Routes = [
     path: 'profilo',
     loadChildren: () =>
       import('./pages/profilo/profilo.module').then((m) => m.ProfiloModule),
+    canActivate: [GuestGuard],
+    canActivateChild: [GuestGuard],
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
   },
 ];
 
